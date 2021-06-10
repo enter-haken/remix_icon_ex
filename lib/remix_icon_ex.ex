ed371748622388b38ce99464e4fd142e0f4cd979
Jan Frederik Hake <jan_hake@gmx.de>
2021-06-10 14:10:14 +0200

    initial commit

---
+++ lib/remix_icon_ex.ex
@@ -0,0 +1,18 @@
+defmodule RemixIconEx do
+  @moduledoc """
+  Documentation for `RemixIconEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RemixIconEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/remix_icon_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule RemixIconExTest do
+  use ExUnit.Case
+  doctest RemixIconEx
+
+  test "greets the world" do
+    assert RemixIconEx.hello() == :world
+  end
+end
